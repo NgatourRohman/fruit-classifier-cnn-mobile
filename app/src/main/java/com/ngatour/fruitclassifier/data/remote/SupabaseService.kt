b27d3e4dcb4dd7a76a93c1f07f4b913e91738cbe
Ngatour Rohman <ngatourrohman25@gmail.com>
2025-05-23 02:32:58 +0700

    Refactor: Extract Supabase headers and config into centralized client

--- app/src/main/java/com/ngatour/fruitclassifier/data/remote/SupabaseService.kt
+++ app/src/main/java/com/ngatour/fruitclassifier/data/remote/SupabaseService.kt
@@ -9,15 +9,8 @@ import retrofit2.http.POST
 
 interface SupabaseService {
     @POST("history")
-    suspend fun uploadHistory(
-        @Body data: List<SupabaseHistory>,
-        @Header("apikey") apiKey: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4eHB1ZmlvcmNqZGRyYXZvc3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTU1NDQsImV4cCI6MjA2MzMzMTU0NH0.L__y0lxToEonPfboB6rAyfplVNoNTgu92iDjOAKeq3c",
-        @Header("Authorization") auth: String = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4eHB1ZmlvcmNqZGRyYXZvc3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTU1NDQsImV4cCI6MjA2MzMzMTU0NH0.L__y0lxToEonPfboB6rAyfplVNoNTgu92iDjOAKeq3c"
-    ): Response<Unit>
+    suspend fun uploadHistory(@Body data: List<SupabaseHistory>): Response<Unit>
 
     @GET("history")
-    suspend fun getHistory(
-        @Header("apikey") apiKey: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4eHB1ZmlvcmNqZGRyYXZvc3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTU1NDQsImV4cCI6MjA2MzMzMTU0NH0.L__y0lxToEonPfboB6rAyfplVNoNTgu92iDjOAKeq3c",
-        @Header("Authorization") auth: String = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4eHB1ZmlvcmNqZGRyYXZvc3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTU1NDQsImV4cCI6MjA2MzMzMTU0NH0.L__y0lxToEonPfboB6rAyfplVNoNTgu92iDjOAKeq3c"
-    ): List<SupabaseHistory>
+    suspend fun getHistory(): List<SupabaseHistory>
 }
