ac024558fc74e82a04f9bb549c72819dad7db8bd
Ngatour Rohman <ngatourrohman25@gmail.com>
2025-05-23 02:26:21 +0700

    Refactor: Centralize Supabase headers using Retrofit interceptor

---
+++ app/src/main/java/com/ngatour/fruitclassifier/data/remote/SupabaseConfig.kt
@@ -0,0 +1,6 @@
+package com.ngatour.fruitclassifier.data.remote
+
+object SupabaseConfig {
+    const val API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4eHB1ZmlvcmNqZGRyYXZvc3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTU1NDQsImV4cCI6MjA2MzMzMTU0NH0.L__y0lxToEonPfboB6rAyfplVNoNTgu92iDjOAKeq3c"
+    const val BASE_URL = "https://txxpufiorcjddravosxp.supabase.co/rest/v1/"
+}
